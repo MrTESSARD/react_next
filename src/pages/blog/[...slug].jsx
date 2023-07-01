d23cab98f97670963bf63b65143844beaead53a4
MrTESSARD <dimitri@tessard.pro>
2023-07-01 19:03:32 +0200

    27. Les liens

---
+++ src/pages/blog/[...slug].jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function slug() {
+  return (
+    <>
+    <div>[...slug]</div>
+    <div>http://localhost:3000/blog/hvj,h/hfghj</div>
+    </>
+  )
+}
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <>
+    <div>Blog</div>
+    <a href="thhps://google.com"></a>
+    </>
+  )
+}
