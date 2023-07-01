d862d140908988440ea23046c0ad2759f947862d
MrTESSARD <dimitri@tessard.pro>
2023-07-01 20:42:31 +0200

    ESLint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Link from "next/link";
-import Navbar from "../components/Navbar/Navbar"
+// import Navbar from "../components/Navbar/Navbar"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <Navbar/>
-        
+                
 
         <h1>Hello</h1>
         <a href="https://google.com">Google</a>
