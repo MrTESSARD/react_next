04ff59e6971119eb905093246e2c494c3f0ce5ed
MrTESSARD <dimitri@tessard.pro>
2023-08-16 19:34:45 +0200

    Nettoyage

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -16,24 +16,29 @@ export default function Index(props: Props) {
   return (
     <div className="container">
       <h1 className={"my-4"}>Bienvenue sur le Blog</h1>
-      <div style={{display:"flex", flexWrap: "wrap", justifyContent: "space-around" }}>
-
-      {props.results.map((item) => (
-        <Card
-          key={item.id}
-          border="primary"
-          style={{ width: "18rem", margin:"1%" }}
-          bg={"secondary"}
-          text={"white"}
-        >
-          <Card.Header>{item.id}</Card.Header>
-          <Card.Body>
-            <Card.Title>{item.title.toUpperCase()}</Card.Title>
-            <Card.Text>{item.body}</Card.Text>
-            <Link href={`/blog/${item.id.toString()}`}>Blog</Link>
-          </Card.Body>
-        </Card>
-      ))}
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+        }}
+      >
+        {props.results.map((item) => (
+          <Card
+            key={item.id}
+            border="primary"
+            style={{ width: "18rem", margin: "1%" }}
+            bg={"secondary"}
+            text={"white"}
+          >
+            <Card.Header>{item.id}</Card.Header>
+            <Card.Body>
+              <Card.Title>{item.title.toUpperCase()}</Card.Title>
+              <Card.Text>{item.body}</Card.Text>
+              <Link href={`/blog/${item.id.toString()}`}>Blog</Link>
+            </Card.Body>
+          </Card>
+        ))}
       </div>
     </div>
   );
